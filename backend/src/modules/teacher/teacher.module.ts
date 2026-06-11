1807747521bcafb5f9dfee2a36a7591c47db1eac
Nikhil <nikhil@example.com>
2026-06-11 23:25:21 +0530

    build

--- backend/src/modules/teacher/teacher.module.ts
+++ backend/src/modules/teacher/teacher.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
+import { PrismaService } from "../../prisma/prisma.service";
 import { TeacherController } from "./teacher.controller";
 import { TeacherService } from "./teacher.service";
 
 @Module({
   controllers: [TeacherController],
-  providers: [TeacherService],
+  providers: [TeacherService, PrismaService],
   exports: [TeacherService],
 })
 export class TeacherModule {}
