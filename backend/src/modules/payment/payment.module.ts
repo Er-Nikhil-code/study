7e04e0427ad9ccac0fa131306850d55eabcb5187
Nikhil <nikhil@example.com>
2026-06-12 18:56:53 +0530

    build

--- backend/src/modules/payment/payment.module.ts
+++ backend/src/modules/payment/payment.module.ts
@@ -2,11 +2,12 @@ import { Module } from "@nestjs/common";
 import { PaymentController } from "./payment.controller";
 import { PaymentService } from "./payment.service";
 import { HierarchyModule } from "../hierarchy/hierarchy.module";
+import { PrismaService } from "../../prisma/prisma.service";
 
 @Module({
   imports: [HierarchyModule],
   controllers: [PaymentController],
-  providers: [PaymentService],
+  providers: [PaymentService, PrismaService],
   exports: [PaymentService]
 })
 export class PaymentModule {}
