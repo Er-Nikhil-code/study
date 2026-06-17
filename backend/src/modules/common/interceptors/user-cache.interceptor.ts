1c514ffb0e446e97c4792908bef3cd23b9b185ae
Nikhil <nikhil@example.com>
2026-06-17 23:04:48 +0530

    test series added foor admin

--- backend/src/modules/common/interceptors/user-cache.interceptor.ts
+++ backend/src/modules/common/interceptors/user-cache.interceptor.ts
@@ -1,8 +1,15 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
-import { CacheInterceptor } from '@nestjs/cache-manager';
+import { ExecutionContext, Injectable, Inject } from '@nestjs/common';
+import { CacheInterceptor, CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class UserCacheInterceptor extends CacheInterceptor {
+  constructor(
+    @Inject(CACHE_MANAGER) cacheManager: any,
+    reflector: Reflector,
+  ) {
+    super(cacheManager, reflector);
+  }
   trackBy(context: ExecutionContext): string | undefined {
     const request = context.switchToHttp().getRequest();
     const isGetRequest = request.method === 'GET';
